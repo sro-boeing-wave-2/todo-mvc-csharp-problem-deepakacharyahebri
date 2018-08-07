f273f70a846e5828e79ab6e87b1fe443c01b44b0
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-08-07 16:26:09 +0530

    put is left in integration test

--- ToDoAssignment.Tests/IntegrationTest.cs
+++ ToDoAssignment.Tests/IntegrationTest.cs
@@ -13,6 +13,8 @@ using Microsoft.AspNetCore.Hosting;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Text;
+using Newtonsoft.Json.Linq;
+using FluentAssertions;
 
 namespace ToDoAssignment.Tests
 {
@@ -51,6 +53,7 @@ namespace ToDoAssignment.Tests
             var notes =
                 new Notes()
                 {
+                    Id = 1,
                     Title = "My First Note",
                     PlainText = "This is my plaintext",
                     PinStatus = true,
@@ -74,9 +77,55 @@ namespace ToDoAssignment.Tests
             var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
             var Response = await _client.PostAsync("/api/notes", stringContent);
             var ResponseGet = await _client.GetAsync("/api/notes");
+            Console.WriteLine(await ResponseGet.Content.ReadAsStringAsync());
             ResponseGet.EnsureSuccessStatusCode();
         }
 
-    }
+        [Fact]
+        public async Task TestGetByIdAfterPost()
+        {
+            var notes =
+                new Notes()
+                {
+                    Id = 1,
+                    Title = "My First Note",
+                    PlainText = "This is my plaintext",
+                    PinStatus = true,
+                    CheckLists = new List<CheckList>()
+                    {
+                        new CheckList()
+                        {
+                            CheckListData="checklist data 1",
+                            ChickListStatus=true
+                        }
+                    },
+                    Labels = new List<Label>()
+                    {
+                        new Label()
+                        {
+                            LabelData ="labeldata 1"
+                        }
+                    }
+                };
+            var json = JsonConvert.SerializeObject(notes);
+            var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+            var Response = await _client.PostAsync("/api/notes", stringContent);
 
+            var ResponseForGetOne = await _client.GetAsync("/api/notes/1");
+            ResponseForGetOne.EnsureSuccessStatusCode();
+
+            var ResponseForGetFirst = await _client.GetAsync("/api/notes/search/First");
+            var result = await ResponseForGetFirst.Content.ReadAsStringAsync();
+            Console.WriteLine(await ResponseForGetFirst.Content.ReadAsStringAsync());
+            var JArrayFormat = JArray.Parse(result);
+            var JObjectNotes = JArrayFormat[0];
+            Assert.Equal(JObjectNotes["id"].ToString(), "1");
+            Assert.Equal(JObjectNotes["title"].ToString(), "My First Note");
+            Assert.Equal(JObjectNotes["plainText"].ToString(), "This is my plaintext");
+            Assert.Equal(JObjectNotes["pinStatus"].ToString(), "True");
+
+            var DeleteResponse = await _client.DeleteAsync("/api/notes/1");
+            DeleteResponse.EnsureSuccessStatusCode();
+        }
+    }
 }
